78ba358392770108169419285650f7ae537b2f5d
Sane4ekF <sanekforever01@gmail.com>
2025-01-27 16:20:43 +0300

    in

---
+++ Mlyaviy_MathTask.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Mlyaviy_MathTask.h"
+
+using namespace std;
+
+int main()
+{
+    string str_input;
+
+
+    cout << "input height A" << endl;
+
+    getline(cin, str_input);
+
+    while (!UserInput(str_input)) {
+        cout << "input height A" << endl;
+
+        getline(cin, str_input);
+    }
+
+
+    int NumberA = stoi(str_input);
+
+    cout << "input height B" << endl;
+    getline(cin, str_input);
+    while (!UserInput(str_input)) {
+        cout << "input height B" << endl;
+        getline(cin, str_input);
+    }
+    
+
+    int NumberB = stoi(str_input);
+
+
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+
+    cout << "Area of Rectangle is " << RectangleArea << endl; 
+}
+
+
+
+
---
+++ Mlyaviy_MathTask.h
@@ -0,0 +1,26 @@
+#ifndef _MLYAVIY_MATHTASK_H_
+#define _MLYAVIY_MATHTASK_H_
+
+#include <cstring>
+#include <string>
+using namespace std;
+
+
+bool UserInput(string input) {
+
+    if (input.empty()) return false;
+
+    try {
+
+        int number = stoi(input);
+    }
+    catch (...)
+    { return false; }
+    return true;
+}
+
+
+int CalcRectangleArea(int NumberA, int NumberB) {
+    return NumberA * NumberB ;
+}
+#endif
